4fcbd665574420419b56ba41b475b1be053f9ebd
ericdunipace <edunipace@mail.harvard.edu>
2025-07-23 00:51:25 -0700

    trying lockfile again

---
+++ renv/set_package_versions.R
@@ -0,0 +1,33 @@
+library(readxl)
+library(glue)
+
+pkg   <- readxl::read_xlsx(here::here("docs","dac-r-and-stata-packages-list-2.xlsx"),
+                               sheet = 4L,
+                           col_type = c("text","text"))
+names <- pkg$Package
+ver   <- pkg$Version
+
+lockfile <- renv::lockfile_read("renv.lock")
+package_names <- names(lockfile$Packages)
+
+final_names <- names[names %in% package_names]
+final_ver   <- ver[names %in% package_names]
+
+final_ver[2] <- "1.1"
+final_ver[38] <- "2.3"
+
+#### Set package versions ####
+for( i in seq_along(final_names)) {
+  n <- final_names[i]
+  v <- final_ver[i]
+  tryCatch(renv::install(glue("{n}@{v}"),
+                lock = TRUE, prompt = FALSE,
+                dependencies = "strong", type = "binary"),
+           error = function(e) {renv::install(glue("{n}"),
+                                              dependencies = "strong", prompt = FALSE,
+                                              lock = TRUE,
+                                              type = "binary")})
+}
+
+renv::snapshot()
+
