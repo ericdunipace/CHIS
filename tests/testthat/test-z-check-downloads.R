17505417d1cdb42848fc52e42ce319cd881aaee7
ericdunipace <edunipace@mail.harvard.edu>
2025-08-04 19:34:23 -0700

    update package checks to make sure installed versions are correct too

--- tests/testthat/test-z-check-downloads.R
+++ tests/testthat/test-z-check-downloads.R
@@ -7,6 +7,9 @@ test_that("All installed packages meet version requirements", {
   names <- pkg$Package
   ver   <- pkg$Version
   
+  rver <- base::getRversion()
+  testthat::skip_if(rver != "4.2.3", "Skipping DAC tests because R version is not 4.2.3 as required for DAC")
+  
   testthat::expect_no_error(pkg)
   lockfile      <- renv::lockfile_read(here::here("renv.lock"))
   package_names <- names(lockfile$Packages)
@@ -23,6 +26,24 @@ test_that("All installed packages meet version requirements", {
     v <- final_ver[i]
     testthat::expect_equivalent(package_ver[which(package_names == n)], v,
                              info = paste("Package", n, "version mismatch: expected", v, "but found", package_ver[which(package_names == n)]))
+  }
+  
+  inst_packages <- installed.packages()
+  
+  package_names <- inst_packages[,"Package"]
+  package_ver   <- inst_packages[,"Version"]
+  
+  final_names   <- names[names %in% package_names]
+  final_ver     <- ver[names %in% package_names]
+  
+  if("askpass" %in% final_names) final_ver[final_names == "askpass"]   <- "1.1"
+  if("gridExtra" %in% final_names) final_ver[final_names == "gridExtra"] <- "2.3"
+  
+  for( i in seq_along(final_names)) {
+    n <- final_names[i]
+    v <- final_ver[i]
+    testthat::expect_equivalent(package_ver[which(package_names == n)], v,
+                                info = paste("Package", n, "version mismatch: expected", v, "but found", package_ver[which(package_names == n)]))
   }
   
 })
